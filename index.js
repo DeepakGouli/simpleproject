bdef8e4105e9e1dd327a3a08dc26188e10e59ccf
DeepakGouli <goulideepak00@gmail.com>
2023-09-13 12:13:43 +0530

    Add css and js

---
+++ index.js
@@ -0,0 +1,23 @@
+// JavaScript code for calculator logic
+        // You can add your JavaScript code here
+        // Calculator logic functions
+        let display = document.getElementById('display');
+
+        function appendToDisplay(value) {
+            display.value += value;
+        }
+        
+        function calculateResult() {
+            try {
+                display.value = eval(display.value);
+            } catch (error) {
+                display.value = 'Error';
+            }
+        }
+        
+        function clearDisplay() {
+            display.value = '';
+        }
+        
+            
+        
